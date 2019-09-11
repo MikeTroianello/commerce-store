34825f8d54c18dd90df00b865356607ff9cc3164
Mike <troianello@protonmail.com>
2019-09-10 17:27:48 -0700

    pictures and hover effect added.

--- .history/src/components/directory/directory.component_20190910165753.jsx
+++ .history/src/components/directory/directory.component_20190910165753.jsx
@@ -32,16 +32,14 @@ export default class Directory extends Component {
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/womens',
-          size: 'large'
+          linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/mens',
-          size: 'large'
+          linkUrl: 'shop/mens'
         }
       ]
     };
@@ -49,10 +47,8 @@ export default class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
-          return (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-          );
+        {this.state.sections.map(({ title, imageUrl, id }) => {
+          return <MenuItem key={id} title={title} imageUrl={imageUrl} />;
         })}
       </div>
     );
